cf0555f69c32949e004e7a486b365e37099cfcf0
Tony Dang <tungtonydang@gmail.com>
2024-10-24 16:04:57 +0100

    Restyle home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import "@/app/ui/global.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { inter } from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </AppRouterCacheProvider>
     </html>
   );
 }
