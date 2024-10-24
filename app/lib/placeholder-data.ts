bfc5a1d2fad046350892088d4fb5b04eaa67342d
Tony Dang <tungtonydang@gmail.com>
2024-10-24 22:09:39 +0100

    Seed DB and fetch data

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -12,15 +12,15 @@ const users = [
 const customers = [
   {
     id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
-    email: 'evil@rabbit.com',
-    image_url: '/customers/evil-rabbit.png',
+    name: 'Emily Davis',
+    email: 'emily@davis.com',
+    image_url: '/customers/emily-davis.png',
   },
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
-    email: 'delba@oliveira.com',
-    image_url: '/customers/delba-de-oliveira.png',
+    name: 'Lara Smith',
+    email: 'lara@smith.com',
+    image_url: '/customers/lara-smith.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
@@ -30,9 +30,9 @@ const customers = [
   },
   {
     id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
-    name: 'Michael Novotny',
-    email: 'michael@novotny.com',
-    image_url: '/customers/michael-novotny.png',
+    name: 'James Brown',
+    email: 'james@brown.com',
+    image_url: '/customers/james-brown.png',
   },
   {
     id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
@@ -42,9 +42,9 @@ const customers = [
   },
   {
     id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
-    name: 'Balazs Orban',
-    email: 'balazs@orban.com',
-    image_url: '/customers/balazs-orban.png',
+    name: 'Chris Johnson',
+    email: 'chris@johnson.com',
+    image_url: '/customers/chris-johnson.png',
   },
 ];
 
