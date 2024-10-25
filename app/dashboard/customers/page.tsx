cdc69add8ed8b32ea74a8f563dca4c7a2edcce9f
Tony Dang <tungtonydang@gmail.com>
2024-10-25 09:08:22 +0100

    Fix typo

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -50,7 +50,7 @@ export default async function Page(props: {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
       </div>
       <div className="my-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search customers..." />
