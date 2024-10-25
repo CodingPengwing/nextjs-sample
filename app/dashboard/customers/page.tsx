19a0e77d7dfcb699ea2221e0b51b37939120c83d
Tony Dang <tungtonydang@gmail.com>
2024-10-25 09:04:30 +0100

    Add customers table with MaterialUI

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,15 +1,62 @@
-import { TextField, FormControl, Input } from "@mui/material";
+import { fetchCustomers } from "@/app/lib/data";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { CustomerField } from "@/app/lib/definitions";
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
 
-export default function Page() {
+function CustomerTable({ customers }: { customers: CustomerField[] }) {
   return (
-    <div>
-      <p>Customers Page</p>
-      {/* <TextField /> */}
-      <Input
-        inputProps={{
-          style: { borderWidth: 0 },
-        }}
-      />
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="customers table">
+        <TableHead className="bg-slate-50">
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">ID</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((customer) => (
+            <TableRow
+              key={customer.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {customer.name}
+              </TableCell>
+              <TableCell align="right">{customer.email}</TableCell>
+              <TableCell align="right">{customer.id}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default async function Page(props: {
+  searchParams?: Promise<{ query?: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchCustomers(query);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="my-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+        {/* <CreateInvoice /> */}
+      </div>
+      <CustomerTable customers={customers} />
     </div>
   );
 }
